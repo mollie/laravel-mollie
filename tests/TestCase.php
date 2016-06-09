5b79e846d38b40852a67ed1784227bfa72ab6f8e
Pierre Tondereau <pierre.tondereau@gmail.com>
2016-06-09 16:58:09 +0200

    Add Wrapper test and fix Socialite tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -21,4 +21,22 @@ abstract class TestCase extends AbstractPackageTestCase
     {
         return MollieServiceProvider::class;
     }
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        $providers = [MollieServiceProvider::class];
+
+        if (interface_exists('Laravel\Socialite\Contracts\Factory')) {
+            $providers[] = Laravel\Socialite\SocialiteServiceProvider::class;
+        }
+
+        return $providers;
+    }
 }
