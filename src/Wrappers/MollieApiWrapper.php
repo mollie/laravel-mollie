5d285a740580f7577349ae27d9e066f419c4bd7d
Julius <jubeki99@gmail.com>
2020-04-02 13:47:22 +0200

    Add dynamic calls via property

--- src/Wrappers/MollieApiWrapper.php
+++ src/Wrappers/MollieApiWrapper.php
@@ -247,4 +247,23 @@ class MollieApiWrapper
     {
         return $this->client->wallets;
     }
+
+    /**
+     * Handle dynamic property calls.
+     *
+     * @param  string $property
+     * @return mixed
+     */
+    public function __get($property)
+    {
+        if (method_exists($this, $property)) {
+            return call_user_func([$this, $property]);
+        }
+
+        $message = '%s does not respond to the "%s" property or method.';
+
+        throw new \Exception(
+            sprintf($message, static::class, $property)
+        );
+    }
 }
