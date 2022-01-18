0bac3f611059b1679391feeaaaa61fcec021eb48
Sander van Hooft <sandervanhooft@users.noreply.github.com>
2022-01-18 12:37:43 +0100

    Update version number

--- src/MollieServiceProvider.php
+++ src/MollieServiceProvider.php
@@ -43,7 +43,7 @@ use Mollie\Laravel\Wrappers\MollieApiWrapper;
  */
 class MollieServiceProvider extends ServiceProvider
 {
-    const PACKAGE_VERSION = '2.16.0';
+    const PACKAGE_VERSION = '2.17.0';
 
     /**
      * Boot the service provider.
