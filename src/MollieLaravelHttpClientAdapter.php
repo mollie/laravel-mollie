a7592c4c5a272b085e52c54bcca965c3c788eeb0
Daniele Rosario <daniele@weble.it>
2022-09-27 13:05:45 +0200

    Http client adapter

---
+++ src/MollieLaravelHttpClientAdapter.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Mollie\Laravel;
+
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
+use Mollie\Api\Exceptions\ApiException;
+use Mollie\Api\HttpAdapter\MollieHttpAdapterInterface;
+
+class MollieLaravelHttpClientAdapter implements MollieHttpAdapterInterface
+{
+    private const HTTP_NO_CONTENT = 204;
+
+    public function send($httpMethod, $url, $headers, $httpBody): object
+    {
+        $response = Http::withHeaders($headers)
+            ->withBody($httpBody, 'application/json')
+            ->send($httpMethod, $url);
+
+        return $this->parseResponseBody($response);
+    }
+
+    private function parseResponseBody(Response $response)
+    {
+        $body = $response->body();
+        if (empty($body)) {
+            if ($response->status() === self::HTTP_NO_CONTENT) {
+                return null;
+            }
+
+            throw new ApiException("No response body found.");
+        }
+
+        $object = @json_decode($body);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new ApiException("Unable to decode Mollie response: '{$body}'.");
+        }
+
+        if ($response->status() >= 400) {
+            throw ApiException::createFromResponse($response->toPsrResponse(), null);
+        }
+
+        return $object;
+    }
+
+    public function versionString(): string
+    {
+        return 'Laravel/HttpClient';
+    }
+}
