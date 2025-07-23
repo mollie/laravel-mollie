751a8a3bec93284bf2cc7eb454d24cbc933d754d
Krishan Koenig <krishan.koenig@googlemail.com>
2025-07-23 16:20:31 +0200

    fix: laravel mollie client adapter

--- src/MollieLaravelHttpClientAdapter.php
+++ src/MollieLaravelHttpClientAdapter.php
@@ -4,30 +4,19 @@ declare(strict_types=1);
 
 namespace Mollie\Laravel;
 
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Facades\Http;
 use Mollie\Api\Contracts\HttpAdapterContract;
 use Mollie\Api\Exceptions\ApiException;
+use Mollie\Api\Exceptions\RetryableNetworkRequestException;
 use Mollie\Api\Http\PendingRequest;
 use Mollie\Api\Http\Response;
-use Mollie\Api\Utils\Factories;
-use Nyholm\Psr7\Factory\Psr17Factory;
+use Mollie\Api\Traits\HasDefaultFactories;
 
 class MollieLaravelHttpClientAdapter implements HttpAdapterContract
 {
-    /**
-     * Get the HTTP factories used by this adapter.
-     */
-    public function factories(): Factories
-    {
-        $psr17Factory = new Psr17Factory;
-
-        return new Factories(
-            $psr17Factory, // RequestFactoryInterface
-            $psr17Factory, // ResponseFactoryInterface
-            $psr17Factory, // StreamFactoryInterface
-            $psr17Factory  // UriFactoryInterface
-        );
-    }
+    use HasDefaultFactories;
 
     /**
      * Get the version string for this HTTP adapter.
@@ -44,46 +33,25 @@ class MollieLaravelHttpClientAdapter implements HttpAdapterContract
      */
     public function sendRequest(PendingRequest $pendingRequest): Response
     {
-        // Get request details from PendingRequest
-        $method = $pendingRequest->method();
-        $url = $pendingRequest->url();
-        $headers = $pendingRequest->headers();
+        $psrRequest = $pendingRequest->createPsrRequest();
 
-        // Execute request handlers from middleware
-        $pendingRequest->executeRequestHandlers();
-
-        // Create PSR-7 request using factories
-        $psrRequest = $this->factories()
-            ->requestFactory
-            ->createRequest($method, $url);
-
-        // Convert headers from ArrayStore to plain array
-        $headersArray = [];
-        foreach ($headers->all() as $key => $value) {
-            $headersArray[$key] = $value;
-        }
-
-        // Configure Laravel HTTP client with headers
-        $httpClient = Http::withHeaders($headersArray);
-
-        // Send request using Laravel HTTP client
         try {
-            $laravelResponse = $httpClient->send($method, $url);
+            $response = Http::withHeaders($pendingRequest->headers()->all())
+                ->withUrlParameters($pendingRequest->query()->all())
+                ->withBody($psrRequest->getBody())
+                ->send(
+                    $pendingRequest->method(),
+                    $pendingRequest->url(),
+                );
 
-            // Convert Laravel response to PSR-7 response
-            $psrResponse = $laravelResponse->toPsrResponse();
+            $psrResponse = $response->toPsrResponse();
 
-            // Create and return Mollie Response
             return new Response($psrResponse, $psrRequest, $pendingRequest);
-        } catch (\Exception $e) {
-            // Create a generic error response
-            $factory = $this->factories()->responseFactory;
-            $psrErrorResponse = $factory->createResponse(500);
-
-            // Create a Mollie Response with the error
-            $errorResponse = new Response($psrErrorResponse, $psrRequest, $pendingRequest);
-
-            throw new ApiException($errorResponse, $e->getMessage(), $e->getCode(), $e);
+        } catch (ConnectionException $e) {
+            throw new RetryableNetworkRequestException($pendingRequest, $e->getMessage(), $e);
+        } catch (RequestException $e) {
+            // RequestExceptions without response are handled by the retryable network request exception
+            return new Response($e->response->toPsrResponse(), $psrRequest, $pendingRequest, $e);
         }
     }
 }
